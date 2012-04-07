6fb1d82412d73172ed0c95e5d4536dc0b96303ae
Jeremy Williams <ctrl.alt.del.121@gmail.com>
2012-04-07 16:20:59 -0400

    coded

--- os1shell.cpp
+++ os1shell.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : os1shell.cpp
 // Author      : Jeremy Williams
-// Version     :
-// Copyright   :
+// Version     : 1.0
 // Description : A custom shell program
+// Sources	   : cplusplus.com (iostream/strtok information)
 //============================================================================
 
 #include <stdio.h>
@@ -12,31 +12,212 @@
 #include <cstring>
 #include <cerrno>
 #include <wait.h>
+#include <termios.h>
+#include <unistd.h>
 using namespace std;
 
+int sigHandleSetup(){
+	//register all signals except Ctrl-/ using registerSignalAction
+
+}
+
+
+void registerSignalAction(int sig, void *func){
+	//create the struct, fill it with data, and call sigaction
+}
+
+void printSignal(int sig){
+	// print the signal associated with the number (use a switch)
+}
+
+void handleCtrlC(int sig){
+	//print the history when ctrl-c is found
+}
+
+void waitOnChildExit(int sig){
+	// call wait when the child exits to prevent zombie processes
+}
+
+void addToHistory(char* command, char** history, int histIndex){
+	//add to history, overwriting oldest one if full
+	if(history[histIndex] != NULL)
+			delete[] history[histIndex];
+	history[histIndex] = new char[65];
+	strcpy(history[histIndex], command);
+
+}
+
+int handleArrows(){
+	int c = getchar();
+	if (c == '['){
+		c = getchar();
+		switch(c){
+		case 'A': return -5;
+		case 'B': return -6;
+		default: return -4;
+		}
+	}
+}
+
+int getKeyPress(){
+	int c = getchar();
+	if(c == 27) return handleArrows();
+	else
+		return c;
+
+}
+
+void clearLine(){
+	cout << string(65, '\b');     //backspaces the line away
+	cout << "OS1Shell -> ";
+
+}
+
+
+void initTerm(){
+	struct termios term;
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ICANON;
+	term.c_lflag &= ~ECHO;
+	tcsetattr(0, TCSANOW, &term);
+}
+
+void resetTerm(){
+	struct termios term;
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag |= ICANON;
+	term.c_lflag |= ECHO;
+	tcsetattr(0, TCSANOW, &term);
+}
+
+void printHistory(char** history, int histIndex){
+	for(int i = histIndex; i<20; i++){
+		if (history[i] != NULL && strlen(history[i]) != 0)
+			cout << history[i] << endl;
+	}
+	for (int i = 0; i<histIndex; i++){
+		if (history[i] != NULL && strlen(history[i]) != 0)
+				cout << history[i] << endl;
+	}
+}
+
 int main() {
+
+	char **history;
+	int histIndex = 0;
+	int histSize = 0;
+	history = new char *[20];
+	for(int i = 0; i<20; i++){
+		history[i]= new char[65];
+	}
+	initTerm();
+	sigHandleSetup();
 	while(1){
-		printf("OS1Shell -> ");
-		char userin[64], *getsReturn;
+		cout << "OS1Shell -> ";
+		char userin[65];
+		char *command = NULL;
 		bool runInBackground = false;
-		getsReturn = fgets(userin, 64, stdin);  //get up to 64 characters from user
-		for(int i = 0; i<int(strlen(userin)); i++){
-			if(userin[i] == '&'){
-				runInBackground = true;
-				userin[i] = 0;
+		initTerm();
+		int count=0;
+		int arrowHistoryPos = -1;
+		int ch=getKeyPress();
+		bool restart = false;
+		bool gotReturn = false;
+
+		while(count<64 && ch!='\n' && ch!='\r'){
+			switch(ch){
+			case 4:
+				cout << endl;
+				resetTerm();
+				return 0;
+			case -5:
+				if (arrowHistoryPos == -1){    //history mode has not been activated
+					arrowHistoryPos = histIndex;
+				}
+			    arrowHistoryPos--;
+			    if (arrowHistoryPos == -1){
+					arrowHistoryPos = histSize-1;    //wrap around arrow index
+				}
+				if(arrowHistoryPos == histIndex){
+					ch = getKeyPress();  //if there is no earlier history, do nothing and wait for next character
+					break;
+				}
+				else{
+					clearLine();
+					char histCommand[65];
+					strcpy(histCommand, history[arrowHistoryPos]);
+					printf(histCommand, stdin);
+					command = history[arrowHistoryPos];
+					count = strlen(history[arrowHistoryPos]) -1;
+
+
+				}
+
+			case -6:
+			    if(arrowHistoryPos == -1){
+					ch = getKeyPress();  //if the up arrow has not been pressed, we can't go down. ignore arrow.
+					break;
+				}
+				arrowHistoryPos++;
+				if (arrowHistoryPos == histSize){
+					arrowHistoryPos = 0;    //wrap around arrow index
+				}
+				else if(arrowHistoryPos == histIndex){
+					clearLine();
+					arrowHistoryPos = -1;
+					break;
+				}
+				else{
+					clearLine();
+					char histCommand[65];
+					strcpy(histCommand, history[arrowHistoryPos]);
+					printf(histCommand, stdin);
+					command = history[arrowHistoryPos];
+					count = strlen(history[arrowHistoryPos]) -1;
+
+				}
+			case -4:
+				// print invalid input, set count to zero, break
+			default:
+			    userin[count]=ch;
+			    count=count+1;
+			    putchar(ch);
+			    ch=getKeyPress();
+
 			}
-			else if(userin[i] == '\n'){
-				userin[i] = 0;
+
+		}
+		cout << endl;
+		userin[count]='\0';
+
+		char line[65];
+
+		if(command == NULL){
+			strcpy(line, userin);
+		}
+		else{
+		    strcpy(line, command);
+		}
+
+
+
+		//getsReturn = fgets(userin, 65, stdin);  //get up to 64 characters from user
+		for(int i = 0; i<int(strlen(line)); i++){
+			if(line[i] == '&'){
+				runInBackground = true;
+				line[i] = 0;
 			}
 		}
-		if (strlen(userin)==0){
+
+
+		if (strlen(line)==0){
 			continue;
 		}
 
 
-		char* tokens[strlen(userin)];
+		char* tokens[strlen(line)];
 		char* token;
-		token = strtok(userin, " ");
+		token = strtok(line, " ");
 		int i = 0;
 		while (token != NULL){
 			tokens[i] = token;
@@ -53,6 +234,14 @@ int main() {
 		//}
 
 		if(strlen(progName)>0){
+			char historyCommand[] = "history";
+			if(strcmp(progName, historyCommand) == 0){
+				printHistory(history, histIndex);
+				continue;
+			}
+			addToHistory(progName, history, histIndex);
+			histIndex = histIndex+1 % 20;
+			if(histSize < 20) histSize++;
 			int pid = fork();
 			if (pid == 0){
 				int status = execvp(progName, tokens);
@@ -65,6 +254,13 @@ int main() {
 			}else;
 		}
 	}
+
+	for( int i = 0 ; i < 20 ; i++ )
+	delete [] history[i] ;
+	delete [] history;
+
+
+	resetTerm();
 	return EXIT_SUCCESS;
 }
 
